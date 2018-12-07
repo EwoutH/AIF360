c5ef7f708a66a55329b14a5b7ad1652b0136fbaf
Samuel Hoffman <shoffman@ibm.com>
2018-12-07 17:56:09 -0500

    implemented lazy import and easier setup config
    lazy importing allows us to safely load modules into the namespace even if they have uninstalled dependencies
    (e.g. `import aif360.algorithms.inprocessing` is fine but `from aif360.algorithms.inprocessing import AdversarialDebiasing` will raise an error if tensorflow is not installed)
    
    added "extras" functionality to setup.py whereby we can do things like `pip install -e .[adversarial_debiasing]` or `pip install -e .[all]` to easily install dependencies.

--- setup.py
+++ setup.py
@@ -7,6 +7,16 @@ test for biases, explanations for these metrics, and algorithms to mitigate bias
 We have developed the package with extensibility in mind. This library is still in development. We encourage the
 contribution of your datasets, metrics, explainers, and debiasing algorithms."""
 
+extras = {'art_classifier': ['adversarial-robustness-toolbox'],
+          'adversarial_debiasing': ['tensorflow==1.1.0'],
+          'disparate_impact_remover':
+              ['networkx==1.11', 'BlackBoxAuditing;python_version>="3"'],
+          'examples': ['jupyter', 'tqdm', 'matplotlib'],
+          'lime_explainer': ['lime'],
+          'lfr': ['numba'],
+          'optim_preproc': ['cvxpy==0.4.11']}
+extras['all'] = [p for pkgs in extras.values() for p in pkgs]
+
 setup(name='aif360',
       version='0.1.1',
       description='IBM AI Fairness 360',
@@ -23,8 +33,9 @@ setup(name='aif360',
           'scipy',
           'pandas==0.23.3',
           'scikit-learn',
-          'numba',
       ],
+      extras_require=extras,
+      tests_require=['pytest', 'nbconvert', 'nbformat'] + extras['examples'],
       package_data={'aif360': ['data/*', 'data/*/*', 'data/*/*/*']},
       include_package_data=True,
       zip_safe=False)
